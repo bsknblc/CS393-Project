3f0d7d72054d0fb88c44d8afba5e0e65b59c0ccc
Başak Balcı <bbalci@kandy.io>
2021-12-17 17:09:09 +0300

    m

---
+++ src/test/java/com/Ozyegin/CS393/Cs393ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ozyegin.CS393;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs393ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
