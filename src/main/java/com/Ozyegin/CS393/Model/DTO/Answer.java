88d227b6923c0d345bdecf95a2ecb9723ba0334d
Başak Balcı <bbalci@kandy.io>
2021-12-17 21:06:41 +0300

    Model updated

--- src/main/java/com/Ozyegin/CS393/Model/DTO/Answer.java
+++ src/main/java/com/Ozyegin/CS393/Model/DTO/Answer.java
@@ -1,4 +1,49 @@
 package com.Ozyegin.CS393.Model.DTO;
 
+import com.Ozyegin.CS393.Model.MyUser;
+import com.Ozyegin.CS393.Model.Question;
+import com.sun.istack.NotNull;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+
 public class Answer {
+
+    private int answerId;
+
+    private String answerText;
+
+    private int voteCount;
+
+    public Answer(){}
+
+    public Answer(int answerId, String answerText, int voteCount) {
+        this.answerId = answerId;
+        this.answerText = answerText;
+        this.voteCount = voteCount;
+    }
+
+    public int getAnswerId() {
+        return answerId;
+    }
+
+    public void setAnswerId(int answerId) {
+        this.answerId = answerId;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
 }
--- src/main/java/com/Ozyegin/CS393/Model/QuestionComment.java
+++ src/main/java/com/Ozyegin/CS393/Model/QuestionComment.java
@@ -1,4 +1,32 @@
 package com.Ozyegin.CS393.Model;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "T_QUESTION_COMMENT")
 public class QuestionComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int commentId;
+
+    @ManyToOne
+    @NotNull
+    @Column(name = "ANSWER")
+    private Answer answer;
+
+    @NotNull
+    @Column(name = "COMMENT_TEXT")
+    private String commentText;
+
+    @ManyToOne
+    @NotNull
+    @Column(name = "USER")
+    private MyUser user;
+
+    @NotNull
+    @Column(name = "VOTE_COUNT")
+    private int voteCount = 0;
+
 }
