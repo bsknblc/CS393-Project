b9b7d2ef0b40dc0e81f8a3a5a9c96ecbd1db96ba
Furkan Kerem Selimoğlu <83104753+furkankeremselimoglu@users.noreply.github.com>
2021-12-23 18:54:53 +0300

    Update AnswerCommentServiceImp.java

--- src/main/java/com/Ozyegin/CS393/Service/AnswerCommentServiceImp.java
+++ src/main/java/com/Ozyegin/CS393/Service/AnswerCommentServiceImp.java
@@ -1,9 +1,13 @@
 package com.Ozyegin.CS393.Service;
 
+import com.Ozyegin.CS393.DTO.AnswerCommentDTO;
 import com.Ozyegin.CS393.Model.AnswerComment;
 import com.Ozyegin.CS393.Repository.AnswerCommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AnswerCommentServiceImp implements AnswerCommentService {
@@ -13,4 +17,21 @@ public class AnswerCommentServiceImp implements AnswerCommentService {
     public AnswerComment save(AnswerComment answerComment) {
         return answerCommentRepository.save(answerComment);
     }
+
+    public List<AnswerCommentDTO> findAll() {
+        List<AnswerComment> answerComments = answerCommentRepository.findAll();
+        List<AnswerCommentDTO> answerCommentDTOS = new ArrayList<AnswerCommentDTO>();
+        for (AnswerComment answerComment: answerComments) {
+            answerCommentDTOS.add(new AnswerCommentDTO(answerComment.getId()));
+        }
+        return answerCommentDTOS;
+    }
+
+    public AnswerCommentDTO findById(int id){
+        AnswerComment answerComment = answerCommentRepository.findById(id);
+        AnswerCommentDTO answerCommentDTO = new AnswerCommentDTO(answerComment.getId());
+        return  answerCommentDTO;
+    }
+
+    public void deleteById(int id){ answerCommentRepository.deleteById(id); }
 }
