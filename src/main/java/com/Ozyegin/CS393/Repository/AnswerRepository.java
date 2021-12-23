0f693fb9fce271cb2f30d016e7042b953217f5e2
cerenyildizdogan <ceren.yildizdogan@ozu.edu.tr>
2021-12-23 16:18:11 +0300

    Answer Service and Repository have been updated

--- src/main/java/com/Ozyegin/CS393/Repository/AnswerRepository.java
+++ src/main/java/com/Ozyegin/CS393/Repository/AnswerRepository.java
@@ -1,4 +1,9 @@
 package com.Ozyegin.CS393.Repository;
 
-public class AnswerRepository {
+import com.Ozyegin.CS393.Model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 }
--- src/main/java/com/Ozyegin/CS393/Service/AnswerServiceImp.java
+++ src/main/java/com/Ozyegin/CS393/Service/AnswerServiceImp.java
@@ -1,4 +1,16 @@
 package com.Ozyegin.CS393.Service;
 
-public class AnswerServiceImp {
+import com.Ozyegin.CS393.Model.Answer;
+import com.Ozyegin.CS393.Repository.AnswerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnswerServiceImp implements AnswerService {
+    @Autowired
+    AnswerRepository answerRepository;
+
+    public Answer save(Answer answer) {
+        return answerRepository.save(answer);
+    }
 }
