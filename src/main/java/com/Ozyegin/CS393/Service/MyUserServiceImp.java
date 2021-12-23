0fe1fded4763461a7cc958dac5cef0e45e29d1f5
Furkan Kerem Selimoğlu <83104753+furkankeremselimoglu@users.noreply.github.com>
2021-12-23 18:55:28 +0300

    Update MyUserServiceImp.java

--- src/main/java/com/Ozyegin/CS393/Service/MyUserServiceImp.java
+++ src/main/java/com/Ozyegin/CS393/Service/MyUserServiceImp.java
@@ -1,9 +1,13 @@
 package com.Ozyegin.CS393.Service;
 
+import com.Ozyegin.CS393.DTO.MyUserDTO;
 import com.Ozyegin.CS393.Model.MyUser;
 import com.Ozyegin.CS393.Repository.MyUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MyUserServiceImp implements MyUserService{
@@ -12,4 +16,22 @@ public class MyUserServiceImp implements MyUserService{
 
     public MyUser save(MyUser myUser) {
         return myUserRepository.save(myUser);}
+
+    public List<MyUserDTO> findAll(){
+        List<MyUser> myUsers = myUserRepository.findAll();
+        List<MyUserDTO> myUserDTOS = new ArrayList<MyUserDTO>();
+        for (MyUser myUser: myUsers) {
+            myUserDTOS.add(new MyUserDTO(myUser.getUserId()));
+        }
+        return myUserDTOS;
+    }
+
+    public MyUserDTO findById(int id){
+        MyUser myUser = myUserRepository.findById(id);
+        MyUserDTO myUserDTO = new MyUserDTO(myUser.getUserId());
+        return myUserDTO;
+    }
+
+    public void deleteById(int id){ myUserRepository.deleteById(id); }
 }
+
