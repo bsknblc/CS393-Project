9018cef1db34dba200f7f492707a5ed7ee05bf2c
cerenyildizdogan <ceren.yildizdogan@ozu.edu.tr>
2021-12-23 16:05:09 +0300

    AnswerComment Repository and Model have been updated

--- src/main/java/com/Ozyegin/CS393/Model/AnswerComment.java
+++ src/main/java/com/Ozyegin/CS393/Model/AnswerComment.java
@@ -1,4 +1,13 @@
 package com.Ozyegin.CS393.Model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "T_ANSWERCOMMENT")
 public class AnswerComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int answerCommentId;
+
+
 }
