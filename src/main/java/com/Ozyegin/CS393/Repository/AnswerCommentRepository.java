1daf7e353cc4cc7ae17e5199618c78eb02fb18c2
Furkan Kerem Selimoğlu <83104753+furkankeremselimoglu@users.noreply.github.com>
2021-12-23 18:51:21 +0300

    Update AnswerCommentRepository.java
    findById method has been added

--- src/main/java/com/Ozyegin/CS393/Repository/AnswerCommentRepository.java
+++ src/main/java/com/Ozyegin/CS393/Repository/AnswerCommentRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnswerCommentRepository extends JpaRepository<AnswerComment, Integer> {
+    public AnswerComment findById(int id);
 }
