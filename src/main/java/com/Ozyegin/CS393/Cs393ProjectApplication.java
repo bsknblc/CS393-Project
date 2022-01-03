4c2663da9cd8f867282c29993b902389d9231164
Furkan Kerem Selimoğlu <furkankeremselimoglu@gmail.com>
2022-01-03 03:48:39 +0300

    swagger fixed and gradle updated.

--- src/main/java/com/Ozyegin/CS393/Cs393ProjectApplication.java
+++ src/main/java/com/Ozyegin/CS393/Cs393ProjectApplication.java
@@ -2,6 +2,18 @@ package com.Ozyegin.CS393;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.Date;
 
 @SpringBootApplication
 public class Cs393ProjectApplication {
@@ -9,5 +21,4 @@ public class Cs393ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Cs393ProjectApplication.class, args);
 	}
-
 }
